90f1ed89ac0cb41fa31003bc5d21601690b647fe
Pham Duc Hiep <pdhiep.real@gmail.com>
2026-03-18 09:35:44 +0700

    Product Detail Screen

---
+++ lib/animation/fadeanimation.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  const FadeAnimation({super.key, required this.delay, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      duration: const Duration(milliseconds: 500),
+      tween: Tween(begin: 0.0, end: 1.0),
+      curve: Curves.easeIn,
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Transform.translate(
+            offset: Offset(0, 30 * (1 - value)),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/main_shell.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SneakersApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SneakersApp extends StatelessWidget {
+  const SneakersApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sneakers App',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme),
       ),
+      home: const MainShell(),
     );
   }
 }
